69b966d7c0846f08ba727f36fba90a3f8c80ed6e
juanpigurina <75141883+juanpigurina@users.noreply.github.com>
2024-06-18 11:04:08 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.post('/api/greet', (req, res) => {
   const { name } = req.body;
   if (name) {
-    res.json({ greeting: `Hello ${name}!` });
+    res.json({ greeting: `Hey there ${name}, you lookin mighty fine today, Big Daddy!` });
   } else {
     res.status(400).json({ error: 'Name is required' });
   }
